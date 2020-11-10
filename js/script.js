d4f101a9768204b8468190a908b8fa7687ee6f03
MarcoB <mrc.bsllt@gmail.com>
2020-11-10 14:39:31 +0100

    completato esercizio

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var nome = prompt("Come ti chiami?");
+// console.log(nome);
+
+var cognome = prompt("Qual è il tuo cognome?");
+// console.log(cognome);
+
+var colore = prompt("Qual è il tuo colore preferito?");
+// console.log(colore);
+
+var annoCorrente = 2020;
+// console.log(annoCorrente);
+
+var anniStringa = prompt("Quanti anni hai?");
+// console.log(anni);
+
+var anniNumero = parseInt(anniStringa);
+// console.log(anniNumero);
+
+var numero = annoCorrente - anniNumero;
+// console.log(numero);
+
+document.getElementById('name').innerHTML = nome;
+document.getElementById('surname').innerHTML = cognome;
+document.getElementById('password').innerHTML = nome + cognome + colore + numero;
